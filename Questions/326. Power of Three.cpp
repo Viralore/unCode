074b9a153ad21a9c2cc758b4bd276d71815b4fed
Yogendra Kumar Saini <yogendra.12109428@gmail.com>
2023-11-17 19:14:07 +0530

    Solution of Q.326 of LeetCode

---
+++ Questions/326. Power of Three.cpp
@@ -0,0 +1,44 @@
+/*
+326. Power of Three
+
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+An integer n is a power of three, if there exists an integer x such that n == 3^x.
+
+Example 1:
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+Input: n = 0
+Output: false
+Explanation: There is no x where 3^x = 0.
+
+Example 3:
+Input: n = -1
+Output: false
+Explanation: There is no x where 3^x = (-1).
+
+
+Constraints:
+-2^31 <= n <= 2^31 - 1
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPowerOfThree(int n)
+{
+    while(n>1 && n%3==0) n/=3;
+    return n==1?true:false;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number to check for the power of three : ";
+    cin>>n;
+    if(isPowerOfThree(n)) cout<<"true";
+    else cout<<"false";
+    return 0;
+}
