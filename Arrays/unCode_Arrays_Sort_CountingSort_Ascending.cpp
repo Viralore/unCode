e52959de962bdcd2cafbeb916936bebf66a64d76
Yogendra Kumar Saini <yogendra.12109428@gmail.com>
2023-12-14 16:58:47 +0530

    Added code for counting sort(in ascending order)

---
+++ Arrays/unCode_Arrays_Sort_CountingSort_Ascending.cpp
@@ -0,0 +1,72 @@
+//Program for Counting Sort (in Ascending order)
+
+//Time Complexity of Counting Sort
+//Worst Case    Average Case    Best Case
+//O(N+K)          O(N+K)          O(N+K)
+//Space Complexity of Counting Sort is O(N)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void countingSort(int arr[], int size)
+{
+    // Find the maximum element in the array
+    int maxElement = *max_element(arr, arr + size);
+
+    // Create a count array to store the count of each element
+    int* count = new int[maxElement + 1]();
+
+    // Count the occurrences of each element in the input array
+    for (int i = 0; i < size; i++)
+    {
+        count[arr[i]]++;
+    }
+
+    // Modify the count array to store the position of each element in the sorted array
+    for (int i = 1; i <= maxElement; i++)
+    {
+        count[i] += count[i - 1];
+    }
+
+    // Create the output array
+    int* output = new int[size];
+
+    // Build the output array using the count array
+    for (int i = size - 1; i >= 0; i--)
+    {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    // Copy the sorted elements back to the original array
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = output[i];
+    }
+
+    // Free the dynamically allocated memory
+    delete[] count;
+    delete[] output;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int* arr = new int[n];
+    cout << "Enter the elements:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    countingSort(arr, n);
+    cout << "Sorted Array (Ascending Order):" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    delete[] arr;
+    return 0;
+}
