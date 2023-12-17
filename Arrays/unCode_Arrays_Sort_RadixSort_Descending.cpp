94d48a0af255259f136be34c545f40ca9d243eeb
Yogendra Kumar Saini <yogendra.12109428@gmail.com>
2023-12-17 12:59:48 +0530

    Added code for Radix sort(in descending order)

---
+++ Arrays/unCode_Arrays_Sort_RadixSort_Descending.cpp
@@ -0,0 +1,84 @@
+// Program for Radix Sort (in Descending order)
+
+// Time Complexity of Radix Sort
+// Worst Case    Average Case    Best Case
+// O(d * (n + k))  O(d * (n + k))  O(d * (n + k))
+// Space Complexity of Radix Sort is O(n + k)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Get the maximum value in the array
+int getMax(int arr[], int size)
+{
+    return *max_element(arr, arr + size);
+}
+
+// Perform counting sort based on the digit represented by exp
+void countingSortDescending(int arr[], int size, int exp) {
+    const int base = 10;  // Radix/base is 10 for decimal numbers
+    vector<int> output(size);
+    vector<int> count(base, 0);
+
+    // Count the occurrences of each digit at the current place value
+    for (int i = 0; i < size; i++)
+    {
+        count[(arr[i] / exp) % base]++;
+    }
+
+    // Modify count to store the position of each element in the sorted array
+    for (int i = base - 2; i >= 0; i--)
+    {
+        count[i] += count[i + 1];
+    }
+
+    // Build the output array using count and arr
+    for (int i = size - 1; i >= 0; i--)
+    {
+        output[count[(arr[i] / exp) % base] - 1] = arr[i];
+        count[(arr[i] / exp) % base]--;
+    }
+
+    // Copy the sorted elements back to the original array
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = output[i];
+    }
+}
+
+// Perform Radix Sort
+void radixSortDescending(int arr[], int size)
+{
+    int maxElement = getMax(arr, size);
+
+    // Perform counting sort for every digit place (exp)
+    for (int exp = 1; maxElement / exp > 0; exp *= 10)
+    {
+        countingSortDescending(arr, size, exp);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int* arr = new int[n];
+    cout << "Enter the elements:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    radixSortDescending(arr, n);
+
+    cout << "Sorted Array (Descending Order):" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+    return 0;
+}
