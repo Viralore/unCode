7f546fc6d91b4d4ed3ad53bc6facbeb97ae03050
Yogendra Kumar Saini <yogendra.12109428@gmail.com>
2024-01-04 12:02:30 +0530

    Added code for insertion at start in Singly Circular Linked List

---
+++ Linked List/unCode_LinkedList_Singly_Circular_Insertion_AtStart.cpp
@@ -0,0 +1,73 @@
+//Program for insertion at start in Singly Circular Linked list
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+void printCircularLinkedList(Node* head)
+{
+    if (head == nullptr)
+    {
+        cout << "Circular Linked List is empty." << endl;
+        return;
+    }
+
+    Node* current = head;
+    do
+    {
+        cout << current->data << " ";
+        current = current->next;
+    } while (current != head);
+    cout << endl;
+}
+
+Node* insertAtStartCircular(Node* head, int x)
+{
+    Node* newNode = new Node(x);
+    if (head == nullptr)
+    {
+        newNode->next = newNode;  // For the first node, point to itself.
+    }
+    else
+    {
+        Node* current = head;
+        while (current->next != head)
+        {
+            current = current->next;
+        }
+        current->next = newNode;
+        newNode->next = head;
+    }
+    return newNode;  // The new node becomes the new head.
+}
+
+int main()
+{
+    Node* head = nullptr;
+    int N, x;
+    cout << "Enter the number of elements in the Circular Linked List: ";
+    cin >> N;
+
+    cout << endl << "Please Enter the elements" << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Enter the element " << (i + 1) << " : ";
+        cin >> x;
+        head = insertAtStartCircular(head, x);
+    }
+
+    cout << endl << "Printing all the Elements of Circular Linked List" << endl;
+    printCircularLinkedList(head);
+    return 0;
+}
