a8cdc332329d28d27dd6dc2f86b70d6382641170
Yogendra Kumar Saini <yogendra.12109428@gmail.com>
2024-01-09 21:14:54 +0530

    Added code for insertion at last in Doubly Circular Linked list

---
+++ Linked List/unCode_LinkedList_Doubly_Circular_Insertion_AtLast.cpp
@@ -0,0 +1,78 @@
+//Program for insertion at last in Doubly Circular Linked List
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
+
+// Function to insert a node at the end of a Doubly Circular Linked List
+Node* insertAtEndDoublyCircular(Node* head, int x)
+{
+    Node* newNode = new Node(x);
+
+    if (head == nullptr)
+    {
+        head = newNode;
+        head->next = head;
+        head->prev = head;
+    }
+    else
+    {
+        Node* last = head->prev;
+        last->next = newNode;
+        newNode->prev = last;
+        newNode->next = head;
+        head->prev = newNode;
+    }
+    return head;
+}
+
+
+void printDoublyCircularLinkedList(Node* head)
+{
+    if (head == nullptr)
+    {
+        cout << "Doubly Circular Linked List is empty." << endl;
+        return;
+    }
+    Node* current = head;
+    do
+    {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    while (current != head);
+    cout << endl;
+}
+
+int main()
+{
+    Node* head = nullptr;
+    int N, x;
+    cout << "Enter the number of elements in the Doubly Circular Linked List: ";
+    cin >> N;
+
+    cout << endl << "Please Enter the elements" << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Enter the element " << (i + 1) << " : ";
+        cin >> x;
+        head = insertAtEndDoublyCircular(head, x);
+    }
+
+    cout << endl << "Printing all the Elements of Doubly Circular Linked List" << endl;
+    printDoublyCircularLinkedList(head);
+    return 0;
+}
