fa9bf0e184abf5285f4b6108b161517d9a035bc0
Yogendra Kumar Saini <yogendra.12109428@gmail.com>
2024-01-13 00:50:58 +0530

    Added code for deletion of last node in doubly circular linked list

--- Linked List/unCode_LinkedList_Doubly_Circular_Deletion_AtLast.cpp
+++ Linked List/unCode_LinkedList_Doubly_Circular_Deletion_AtLast.cpp
@@ -1,4 +1,4 @@
-//Program for deletion at start in Doubly Circular Linked List
+//Program for deletion at last in Doubly Circular Linked List
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -41,8 +41,8 @@ Node* insertAtStartDoublyCircular(Node* head, int x)
     return head;
 }
 
-// Function to delete the first node in a Doubly Circular Linked List
-Node* deleteAtStartDoublyCircular(Node* head)
+// Function to delete the last node in a Doubly Circular Linked List
+Node* deleteAtEndDoublyCircular(Node* head)
 {
     if (head == nullptr)
     {
@@ -51,6 +51,7 @@ Node* deleteAtStartDoublyCircular(Node* head)
     }
 
     Node* temp = head;
+    Node* last = head->prev;
 
     if (head->next == head)
     { // Only one node in the list
@@ -58,14 +59,13 @@ Node* deleteAtStartDoublyCircular(Node* head)
         return nullptr;
     }
 
-    Node* last = head->prev;
-    head = head->next;
-    last->next = head;
-    head->prev = last;
+    last->prev->next = head;
+    head->prev = last->prev;
 
-    delete temp;
+    delete last;
     return head;
 }
+
 
 
 void printDoublyCircularLinkedList(Node* head)
@@ -100,11 +100,11 @@ int main()
         head = insertAtStartDoublyCircular(head, x);
     }
 
-    cout << endl << "Printing all the Elements of Doubly Circular Linked List Before Deleting Start Node" << endl;
+    cout << endl << "Printing all the Elements of Doubly Circular Linked List Before Deleting Last Node" << endl;
     printDoublyCircularLinkedList(head);
 
-    cout << endl << "Printing all the Elements of Doubly Circular Linked List After Deleting Start Node" << endl;
-    head = deleteAtStartDoublyCircular(head);
+    cout << endl << "Printing all the Elements of Doubly Circular Linked List After Deleting Last Node" << endl;
+    head = deleteAtEndDoublyCircular(head);
     printDoublyCircularLinkedList(head);
     return 0;
 }
