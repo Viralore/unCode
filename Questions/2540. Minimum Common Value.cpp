3bd89e66abbe27753919654d081025e086dd3223
Yogendra <yogendra.12109428@gmail.com>
2024-03-09 20:53:54 +0530

    Solution of LeetCode 2540

---
+++ Questions/2540. Minimum Common Value.cpp
@@ -0,0 +1,38 @@
+/*
+2540. Minimum Common Value
+
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays.
+If there is no common integer amongst nums1 and nums2, return -1.
+Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+Example 1:
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return 2.
+
+Example 2:
+Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+Output: 2
+Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so 2 is returned.
+
+
+Constraints:
+1 <= nums1.length, nums2.length <= 10^5
+1 <= nums1[i], nums2[j] <= 10^9
+Both nums1 and nums2 are sorted in non-decreasing order.
+*/
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2)
+    {
+        set<int> s1,s2;
+        for(int i : nums1) s1.insert(i);
+        for(int i : nums2) s2.insert(i);
+        for(int i : s1)
+        {
+            if(s2.count(i)==1) return i;
+        }
+        return -1;
+    }
+};
