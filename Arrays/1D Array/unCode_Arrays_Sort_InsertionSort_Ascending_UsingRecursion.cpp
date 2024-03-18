7f292e01ac6b521c13e469246b880e7538a1ca75
Yogendra <yogendra.12109428@gmail.com>
2024-03-18 19:28:53 +0530

    Added code for insertion sort (in ascending order) using recursion

---
+++ Arrays/1D Array/unCode_Arrays_Sort_InsertionSort_Ascending_UsingRecursion.cpp
@@ -0,0 +1,41 @@
+//Program for insertion sort (in ascending order) using recursion
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void insert(vector<int>& arr, int n)
+{
+    if (n <= 1)
+        return;
+
+    insert(arr, n - 1);
+
+    int last = arr[n - 1];
+    int j = n - 2;
+
+    while (j >= 0 && arr[j] > last)
+    {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = last;
+}
+
+void insertionSort(vector<int>& arr)
+{
+    insert(arr, arr.size());
+}
+
+int main()
+{
+    vector<int> arr = {12, 11, 13, 5, 6};
+    insertionSort(arr);
+
+    cout << "Sorted array: ";
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
