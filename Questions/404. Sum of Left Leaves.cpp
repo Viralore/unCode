a6e07efeb267edc048abab6168112a64b3511bf6
Yogendra <yogendra.12109428@gmail.com>
2024-04-15 10:48:45 +0530

    Solution of Q.404 of LeetCode

---
+++ Questions/404. Sum of Left Leaves.cpp
@@ -0,0 +1,53 @@
+/*
+404. Sum of Left Leaves
+Given the root of a binary tree, return the sum of all left leaves.
+A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+
+Example 2:
+Input: root = [1]
+Output: 0
+
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-1000 <= Node.val <= 1000
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) 
+    {
+        if(root==nullptr) return 0;
+        int ans = 0;
+        if(root->left)
+        {
+            if(root->left->left == nullptr && root->left->right == nullptr)
+            {
+                ans += root->left->val;
+            }
+            else ans += sumOfLeftLeaves(root->left);
+        }
+        ans += sumOfLeftLeaves(root->right);
+
+        return ans;
+    }
+};
