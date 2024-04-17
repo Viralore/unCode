c6933f05a8eb24496ae6f68b31be824a4fc38d01
Yogendra <yogendra.12109428@gmail.com>
2024-04-17 22:09:47 +0530

    Added solution of Q.988 of LeetCode

---
+++ Questions/988. Smallest String Starting From Leaf.cpp
@@ -0,0 +1,66 @@
+/*
+988. Smallest String Starting From Leaf
+You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
+Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+As a reminder, any shorter prefix of a string is lexicographically smaller.
+For example, "ab" is lexicographically smaller than "aba".
+A leaf of a node is a node that has no children.
+
+Example 1:
+Input: root = [0,1,2,3,4,3,4]
+Output: "dba"
+
+Example 2:
+Input: root = [25,1,3,1,3,0,2]
+Output: "adz"
+
+Example 3:
+Input: root = [2,2,1,null,1,0,null,0]
+Output: "abc"
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [1, 8500].
+0 <= Node.val <= 25
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    void dfs(TreeNode* root, string&& path, string& ans) 
+    {
+        if (root == nullptr) return;
+        path.push_back(root->val + 'a');
+
+        if (root->left == nullptr && root->right == nullptr) 
+        {
+            reverse(path.begin(), path.end());
+            if (ans == "" || ans > path) ans = path;
+            reverse(path.begin(), path.end());
+        }
+
+        dfs(root->left, move(path), ans);
+        dfs(root->right, move(path), ans);
+        path.pop_back();
+    }
+    string smallestFromLeaf(TreeNode* root) 
+    {
+        string ans;
+        dfs(root, "", ans);
+        return ans;
+    }
+};
